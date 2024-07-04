da179ad36a91127219b07605ca42e82db2ee16c6
a-craker <aidan.craker@gmail.com>
2024-07-04 10:18:43 +0200

    Final script with comments

--- scripts/ncr_data_conversion_script.R
+++ scripts/ncr_data_conversion_script.R
@@ -6,6 +6,12 @@ library(janitor)
 library(tidyverse)
 
 
+# FUNCTION SPEC ---------------------------------------------------------------
+
+#> "mfsa_read_transform" reads in each sheet of the data (different credit product types), combines the products and 
+#> transforms the data to a long format
+
+
 mfsa_read_transform <- function(sheet_name) {
   
   df <- read_xlsx("data/mfsa_credit_product_by_income.xlsx", sheet = sheet_name)
@@ -25,14 +31,25 @@ mfsa_read_transform <- function(sheet_name) {
   
 }
 
-# read in CPI and add to function for transform
+
+# READ DATA ---------------------------------------------------------------
+
+#> The following reads the data, as well as joins in the CPI sheet that is necessary for inflation adjusting
 
 mfsa_allproduct_income <- excel_sheets("data/mfsa_credit_product_by_income.xlsx") %>% 
   .[2:length(.)] %>% 
   map_dfr(
     .,
     mfsa_read_transform
-  ) %>% left_join(cpi, by = "year") 
-
-
-write_xlsx(mfsa_allproduct_income, "output/mfsa_allproduct_income.xlsx")
+  ) %>%  
+  left_join(
+    
+    (read_xlsx("data/mfsa_credit_product_by_income.xlsx", sheet = "CPI") %>% 
+       rename(year = 1, 
+              average_cpi = Average) %>%
+       filter(year >= 2007 & year <= 2023) %>% 
+       select(year, average_cpi) %>% 
+       mutate(year = as.character(year))
+    ), 
+    by = "year") %>%  
+  write_xlsx(mfsa_allproduct_income, "output/mfsa_allproduct_income.xlsx")
